29a60da4235daedef8bc976795c4093c823a50eb
tuongnc <s3818196@rmit.edu.vn>
2020-04-14 23:25:51 +0700

    update booking form

--- a3/script.js
+++ a3/script.js
@@ -7,10 +7,11 @@ var movieList ={
         "description":"After the devastating events of Avengers: Infinity War (2018), the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos's actions and undo the chaos to the universe, no matter what consequences may be in store, and no matter who they face...",
         "link":"https://www.youtube.com/embed/TcMBFSGVi1c",
         "schedule": [
-            { "day":"WED","time":"9","period":"P.M" },
-            { "day":"FRI","time":"9","period":"P.M" },
-            { "day":"SAT","time":"6","period":"P.M" },
-            { "day":"SUN","time":"6","period":"P.M" }
+            { "day":"WED","time":"9 P.M","period":"T21" },
+            { "day":"THU","time":"9 P.M","period":"T21" },
+            { "day":"FRI","time":"9 P.M","period":"T21" },
+            { "day":"SAT","time":"6 P.M","period":"T18" },
+            { "day":"SUN","time":"6 P.M","period":"T18" }
         ]
     },
     Wedding:{
@@ -20,10 +21,10 @@ var movieList ={
         "description":"Lauren and Ned are engaged, they are in love, and they have just ten days to find Lauren's mother who has gone AWOL somewhere in the remote far north of Australia, reunite her parents and pull off their dream wedding.",
         "link":"https://www.youtube.com/embed/uoDBvGF9pPU",
         "schedule": [
-            { "day":"MON","time":"6","period":"P.M" },
-            { "day":"TUE","time":"6","period":"P.M" },
-            { "day":"SAT","time":"3","period":"P.M" },
-            { "day":"SUN","time":"3","period":"P.M" }
+            { "day":"MON","time":"6 P.M","period":"T18" },
+            { "day":"TUE","time":"6 P.M","period":"T18" },
+            { "day":"SAT","time":"3 P.M","period":"T15" },
+            { "day":"SUN","time":"3 P.M","period":"T15" }
         ]
     },
     Dumbo:{
@@ -33,12 +34,13 @@ var movieList ={
         "description":"A young elephant, whose oversized ears enable him to fly, helps save a struggling circus, but when the circus plans a new venture, Dumbo and his friends discover dark secrets beneath its shiny veneer.",
         "link":"https://www.youtube.com/embed/7NiYVoqBt-8",
         "schedule": [
-            { "day":"MON","time":"12","period":"P.M" },
-            { "day":"TUE","time":"12","period":"P.M" },
-            { "day":"WED","time":"6","period":"P.M" },
-            { "day":"FRI","time":"6","period":"P.M" },
-            { "day":"SAT","time":"12","period":"P.M" },
-            { "day":"SUN","time":"12","period":"P.M" }
+            { "day":"MON","time":"12 P.M","period":"T12" },
+            { "day":"TUE","time":"12 P.M","period":"T12" },
+            { "day":"WED","time":"6 P.M","period":"T18" },
+            { "day":"THU","time":"6 P.M","period":"T18" },
+            { "day":"FRI","time":"6 P.M","period":"T18" },
+            { "day":"SAT","time":"12 P.M","period":"T12" },
+            { "day":"SUN","time":"12 P.M","period":"T12" }
         ]
     },
     Prince:{
@@ -48,17 +50,19 @@ var movieList ={
         "description":"The untold story of the last days in the tragic times of Oscar Wilde, a person who observes his own failure with ironic distance and regards the difficulties that beset his life with detachment and humor.",
         "link":"https://www.youtube.com/embed/4HmN9r1Fcr8",
         "schedule": [          
-            { "day":"WED","time":"12","period":"P.M" },
-            { "day":"FRI","time":"12","period":"P.M" },
-            { "day":"SAT","time":"9","period":"P.M" },
-            { "day":"SUN","time":"9","period":"P.M" }
+            { "day":"WED","time":"12 P.M","period":"T12" },
+            { "day":"THU","time":"12 P.M","period":"T12" },
+            { "day":"FRI","time":"12 P.M","period":"T12" },
+            { "day":"SAT","time":"9 P.M","period":"T21" },
+            { "day":"SUN","time":"9 P.M","period":"T21" }
         ]
     }
 };
 // function show detail in SYNOPSIS PANEL
 function showDetail(key) {
     // variables
-
+    let movie_id =movieList[key].id;
+    let movie_name =movieList[key].name;
     // Title +Rating
     document.getElementById('synopsisTitle').innerHTML= movieList[key].name  ;
     document.getElementById('synopsisRating').innerHTML= movieList[key].rating ;
@@ -68,11 +72,14 @@ function showDetail(key) {
     // btn Group
     var newBtnGroup = '<p style="padding-right:50px;  font-size: 20px">Making a book:</p>';
     for (const i in movieList[key].schedule) {
-        let dayMovie = movieList[key].schedule[i].day ;
-        let timeMovie = movieList[key].schedule[i].time ;
-        let periodMovie= movieList[key].schedule[i].period ;
-        newBtnGroup += ' <a  class="btn btn-info mr-3" style="margin-bottom: 10px" href="#Booking">' +dayMovie +" - "+ timeMovie +" "+periodMovie + '</a>';
-         
+        let movie_day = movieList[key].schedule[i].day ;
+        let movie_time = movieList[key].schedule[i].time ;
+        let movie_period= movieList[key].schedule[i].period ;
+        newBtnGroup += ' <a  class="btn btn-info mr-3" style="margin-bottom: 10px" onclick="showBooking(\''+movie_id +'\',\''+ movie_name +'\',\''+ movie_day +'\',\''+ movie_time +'\',\''+movie_period  + '\');" href="#Booking">' +movie_day +" - "+ movie_time +'</a>';       
     }
     document.getElementById('sysnopisBtnGroup').innerHTML= newBtnGroup ;
+}
+// function show detail in BOOKING FORM
+function showBooking(movie_id,movie_name,movie_day,movie_time,movie_period) {
+    console.log(movie_id,movie_name,movie_day,movie_time,movie_period);
 }
